1139a0b7d622127f7711fc16b99eed7836ad8efb
KaydenLe <kayden3196@gmail.com>
2024-10-29 09:22:33 -0700

    comments v1?

--- src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/Comments.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/Comments.java
@@ -1,11 +1,59 @@
 package com.nighthawk.spring_portfolio.mvc.mortevision;
 
-public class Comments {
-    private long Assignmentid;
-    private long id;
+class Comment {
+    private String user;
+    private String text;
+    
+    public Comment(String user, String text) {
+        this.user = user;
+        this.text = text;
+    }
 
-    private String comment;
+    public String getUser() {
+        return user;
+    }
 
+    public String getText() {
+        return text;
+    }
 
-
+    @Override
+    public String toString() {
+        return "Comment by " + user + ": " + text;
+    }
 }
+
+class Assignment {
+    private int id;
+    private String title;
+    private List<Comment> comments;
+
+    public Assignment(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.comments = new ArrayList<>();
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment " + id + ": " + title;
+    }
+}
+
