52b04e362506e6914ca5116404ebaabd09fa1475
KaydenLe <kayden3196@gmail.com>
2024-10-29 09:30:49 -0700

    no assignemnt

--- src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/Comments.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/Comments.java
@@ -22,38 +22,3 @@ class Comment {
         return "Comment by " + user + ": " + text;
     }
 }
-
-class Assignment {
-    private int id;
-    private String title;
-    private List<Comment> comments;
-
-    public Assignment(int id, String title) {
-        this.id = id;
-        this.title = title;
-        this.comments = new ArrayList<>();
-    }
-
-    public void addComment(Comment comment) {
-        comments.add(comment);
-    }
-
-    // Getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    @Override
-    public String toString() {
-        return "Assignment " + id + ": " + title;
-    }
-}
-
