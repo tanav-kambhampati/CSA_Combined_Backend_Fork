3e0c26fed9961a2ae1597b695505408395aa8d6c
KinetekEnergy <transyt8@gmail.com>
2024-10-31 12:36:53 -0700

    added create account stuff

--- src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
@@ -13,6 +13,19 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 /*
 * To enable HTTP Security in Spring
 */
+
+/*
+ * THIS FILE IS IMPORTANT
+ * 
+ * you can configure which http requests need to be authenticated or not
+ * for example, you can change the /authenticate to "authenticated()" or "permitAll()"
+ * --> obviously, you want to set it to permitAll() so anyone can login. it doesn't make sense
+ *     to have to login first before authenticating!
+ * 
+ * another example is /mvc/person/create/** which i changed to permitAll() so anyone can make an account.
+ * it doesn't make sense to have to login to make your account!
+ */
+
 @Configuration
 public class SecurityConfig {
 
@@ -23,55 +36,51 @@ public class SecurityConfig {
     private JwtRequestFilter jwtRequestFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {	
-        
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-            // JWT related configuration
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.POST,"/authenticate").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/person/**").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/person/**").authenticated()
-                .requestMatchers(HttpMethod.GET, "/api/people/**").authenticated()
-                .requestMatchers(HttpMethod.PUT, "/api/person/**").authenticated()
-                .requestMatchers(HttpMethod.DELETE, "/api/person/**").hasAuthority("ROLE_ADMIN")
-            )
-            .cors(Customizer.withDefaults())
-            .headers(headers -> headers
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET", "DELETE", "OPTIONS", "HEAD"))
-            )
-			.exceptionHandling(exceptions -> exceptions
-				.authenticationEntryPoint(jwtAuthenticationEntryPoint)
-			)
-			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-            
-			// Session related configuration
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/mvc/person/search/**").authenticated()
-                .requestMatchers("/mvc/person/create/**").authenticated()
-                .requestMatchers("/mvc/person/read/**").authenticated()
-                .requestMatchers("/mvc/person/update/**").authenticated()
-                .requestMatchers("/mvc/person/delete/**").hasAuthority("ROLE_ADMIN")
-                .requestMatchers(HttpMethod.GET,"/login").permitAll()
-                .requestMatchers(HttpMethod.POST,"/authenticateForm").permitAll()
-                .requestMatchers("/**").permitAll()
-            )
-            .formLogin(form -> form 
-                .loginPage("/login")
-                .defaultSuccessUrl("/mvc/person/read")
-            )
-            .logout(logout -> logout
-                .deleteCookies("sess_java_spring")
-                .logoutSuccessUrl("/")
-            )
-			;
+                // JWT related configuration
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/person/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/person/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/people/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/person/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/person/**").hasAuthority("ROLE_ADMIN"))
+                .cors(Customizer.withDefaults())
+                .headers(headers -> headers
+                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
+                        .addHeaderWriter(
+                                new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
+                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type",
+                                "Authorization", "x-csrf-token"))
+                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
+                        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET",
+                                "DELETE", "OPTIONS", "HEAD")))
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+
+                // Session related configuration
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/mvc/person/search/**").authenticated()
+                        .requestMatchers("/mvc/person/create/**").permitAll()
+                        .requestMatchers("/mvc/person/read/**").authenticated()
+                        .requestMatchers("/mvc/person/update/**").authenticated()
+                        .requestMatchers("/mvc/person/delete/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/authenticateForm").permitAll()
+                        .requestMatchers("/**").permitAll())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/mvc/person/read"))
+                .logout(logout -> logout
+                        .deleteCookies("sess_java_spring")
+                        .logoutSuccessUrl("/"));
         return http.build();
     }
 }
