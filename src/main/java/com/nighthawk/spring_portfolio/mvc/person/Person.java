d9f781f55a3464425dffa01ef762d9d6da65ab14
KinetekEnergy <transyt8@gmail.com>
2024-10-31 18:20:39 -0700

    ctrl a ctrl k ctrl f

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Person.java
@@ -37,47 +37,64 @@ import lombok.NonNull;
 
 /**
  * Person is a POJO, Plain Old Java Object.
- * --- @Data is Lombox annotation for @Getter @Setter @ToString @EqualsAndHashCode @RequiredArgsConstructor
- * --- @AllArgsConstructor is Lombox annotation for a constructor with all arguments
- * --- @NoArgsConstructor is Lombox annotation for a constructor with no arguments
+ * --- @Data is Lombox annotation
+ * for @Getter @Setter @ToString @EqualsAndHashCode @RequiredArgsConstructor
+ * --- @AllArgsConstructor is Lombox annotation for a constructor with all
+ * arguments
+ * --- @NoArgsConstructor is Lombox annotation for a constructor with no
+ * arguments
  * --- @Entity annotation is used to mark the class as a persistent Java class.
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Convert(attributeName ="person", converter = JsonType.class)
+@Convert(attributeName = "person", converter = JsonType.class)
 public class Person {
 
-    /** automatic unique identifier for Person record
+    /**
+     * automatic unique identifier for Person record
      * --- Id annotation is used to specify the identifier property of the entity.
-     * ----GeneratedValue annotation is used to specify the primary key generation strategy to use.
-     * ----- The strategy is to have the persistence provider pick an appropriate strategy for the particular database.
-     * ----- GenerationType.AUTO is the default generation type and it will pick the strategy based on the used database.
-     */ 
+     * ----GeneratedValue annotation is used to specify the primary key generation
+     * strategy to use.
+     * ----- The strategy is to have the persistence provider pick an appropriate
+     * strategy for the particular database.
+     * ----- GenerationType.AUTO is the default generation type and it will pick the
+     * strategy based on the used database.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    /** Many to Many relationship with PersonRole
-     * --- @ManyToMany annotation is used to specify a many-to-many relationship between the entities.
-     * --- FetchType.EAGER is used to specify that data must be eagerly fetched, meaning that it must be loaded immediately.
-     * --- Collection is a root interface in the Java Collection Framework, in this case it is used to store PersonRole objects.
-     * --- ArrayList is a resizable array implementation of the List interface, allowing all elements to be accessed using an integer index.
-     * --- PersonRole is a POJO, Plain Old Java Object. 
+    /**
+     * Many to Many relationship with PersonRole
+     * --- @ManyToMany annotation is used to specify a many-to-many relationship
+     * between the entities.
+     * --- FetchType.EAGER is used to specify that data must be eagerly fetched,
+     * meaning that it must be loaded immediately.
+     * --- Collection is a root interface in the Java Collection Framework, in this
+     * case it is used to store PersonRole objects.
+     * --- ArrayList is a resizable array implementation of the List interface,
+     * allowing all elements to be accessed using an integer index.
+     * --- PersonRole is a POJO, Plain Old Java Object.
      */
     @ManyToMany(fetch = EAGER)
     private Collection<PersonRole> roles = new ArrayList<>();
 
-    /** email, password, roles are key attributes to login and authentication
-     * --- @NotEmpty annotation is used to validate that the annotated field is not null or empty, meaning it has to have a value.
-     * --- @Size annotation is used to validate that the annotated field is between the specified boundaries, in this case greater than 5.
-     * --- @Email annotation is used to validate that the annotated field is a valid email address.
-     * --- @Column annotation is used to specify the mapped column for a persistent property or field, in this case unique and email.
+    /**
+     * email, password, roles are key attributes to login and authentication
+     * --- @NotEmpty annotation is used to validate that the annotated field is not
+     * null or empty, meaning it has to have a value.
+     * --- @Size annotation is used to validate that the annotated field is between
+     * the specified boundaries, in this case greater than 5.
+     * --- @Email annotation is used to validate that the annotated field is a valid
+     * email address.
+     * --- @Column annotation is used to specify the mapped column for a persistent
+     * property or field, in this case unique and email.
      */
     @NotEmpty
-    @Size(min=5)
-    @Column(unique=true)
+    @Size(min = 5)
+    @Column(unique = true)
     @Email
     private String email;
 
@@ -88,11 +105,15 @@ public class Person {
     @Size(min = 7, max = 7, message = "MyPlan username must be your 7 digit ID number.")
     private String myplan;
 
-    /** name, dob are attributes to describe the person
-     * --- @NonNull annotation is used to generate a constructor with AllArgsConstructor Lombox annotation.
-     * --- @Size annotation is used to validate that the annotated field is between the specified boundaries, in this case between 2 and 30 characters.
-     * --- @DateTimeFormat annotation is used to declare a field as a date, in this case the pattern is specified as yyyy-MM-dd.
-     */ 
+    /**
+     * name, dob are attributes to describe the person
+     * --- @NonNull annotation is used to generate a constructor with
+     * AllArgsConstructor Lombox annotation.
+     * --- @Size annotation is used to validate that the annotated field is between
+     * the specified boundaries, in this case between 2 and 30 characters.
+     * --- @DateTimeFormat annotation is used to declare a field as a date, in this
+     * case the pattern is specified as yyyy-MM-dd.
+     */
     @NonNull
     @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
     private String name;
@@ -100,23 +121,27 @@ public class Person {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
 
-    /** stats is used to store JSON for daily stat$
-     * --- @JdbcTypeCode annotation is used to specify the JDBC type code for a column, in this case json.
-     * --- @Column annotation is used to specify the mapped column for a persistent property or field, in this case columnDefinition is specified as jsonb.
+    /**
+     * stats is used to store JSON for daily stat$
+     * --- @JdbcTypeCode annotation is used to specify the JDBC type code for a
+     * column, in this case json.
+     * --- @Column annotation is used to specify the mapped column for a persistent
+     * property or field, in this case columnDefinition is specified as jsonb.
      * * * Example of JSON data:
-        "stats": {
-            "2022-11-13": {
-                "calories": 2200,
-                "steps": 8000
-            }
-        }
-    */
+     * "stats": {
+     * "2022-11-13": {
+     * "calories": 2200,
+     * "steps": 8000
+     * }
+     * }
+     */
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb")
-    private Map<String,Map<String, Object>> stats = new HashMap<>(); 
-    
+    private Map<String, Map<String, Object>> stats = new HashMap<>();
 
-    /** Custom constructor for Person when building a new Person object from an API call
+    /**
+     * Custom constructor for Person when building a new Person object from an API
+     * call
      */
     public Person(String email, String password, String myplan, String name, Date dob, PersonRole role) {
         this.email = email;
@@ -127,12 +152,14 @@ public class Person {
         this.roles.add(role);
     }
 
-    /** Custom getter to return age from dob attribute
-    */
+    /**
+     * Custom getter to return age from dob attribute
+     */
     public int getAge() {
         if (this.dob != null) {
             LocalDate birthDay = this.dob.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-            return Period.between(birthDay, LocalDate.now()).getYears(); }
+            return Period.between(birthDay, LocalDate.now()).getYears();
+        }
         return -1;
     }
 
@@ -145,15 +172,19 @@ public class Person {
      * @param myplan
      * @param dob
      * @return Person
-     */ 
+     */
     public static Person createPerson(String name, String email, String password, String myplan, String dob) {
         // By default, Spring Security expects roles to have a "ROLE_" prefix.
         return createPerson(name, email, password, myplan, dob, Arrays.asList("ROLE_USER"));
     }
-    /** 2nd telescoping method to create a Person object with parameterized roles
-     * @param roles 
+
+    /**
+     * 2nd telescoping method to create a Person object with parameterized roles
+     * 
+     * @param roles
      */
-    public static Person createPerson(String name, String email, String password, String myplan, String dob, List<String> roleNames) {
+    public static Person createPerson(String name, String email, String password, String myplan, String dob,
+            List<String> roleNames) {
         Person person = new Person();
         person.setName(name);
         person.setEmail(email);
@@ -166,32 +197,38 @@ public class Person {
         } catch (Exception e) {
             // handle exception
         }
-    
+
         List<PersonRole> roles = new ArrayList<>();
         for (String roleName : roleNames) {
             PersonRole role = new PersonRole(roleName);
             roles.add(role);
         }
         person.setRoles(roles);
-    
+
         return person;
     }
-   
-    /** Static method to initialize an array list of Person objects 
+
+    /**
+     * Static method to initialize an array list of Person objects
+     * 
      * @return Person[], an array of Person objects
      */
     public static Person[] init() {
         ArrayList<Person> persons = new ArrayList<>();
-        persons.add(createPerson("Thomas Edison", "toby@gmail.com", "123toby", "0000000", "01-01-1840", Arrays.asList("ROLE_ADMIN", "ROLE_USER", "ROLE_TESTER")));
+        persons.add(createPerson("Thomas Edison", "toby@gmail.com", "123toby", "0000000", "01-01-1840",
+                Arrays.asList("ROLE_ADMIN", "ROLE_USER", "ROLE_TESTER")));
         persons.add(createPerson("Alexander Graham Bell", "lexb@gmail.com", "123lex", "0000000", "01-01-1847"));
         persons.add(createPerson("Nikola Tesla", "niko@gmail.com", "123niko", "0000000", "01-01-1850"));
         persons.add(createPerson("Madam Currie", "madam@gmail.com", "123madam", "0000000", "01-01-1860"));
         persons.add(createPerson("Grace Hopper", "hop@gmail.com", "123hop", "0000000", "12-09-1906"));
-        persons.add(createPerson("John Mortensen", "jm1021@gmail.com", "123Qwerty!", "0000000", "10-21-1959", Arrays.asList("ROLE_ADMIN")));
+        persons.add(createPerson("John Mortensen", "jm1021@gmail.com", "123Qwerty!", "0000000", "10-21-1959",
+                Arrays.asList("ROLE_ADMIN")));
         return persons.toArray(new Person[0]);
     }
 
-    /** Static method to print Person objects from an array
+    /**
+     * Static method to print Person objects from an array
+     * 
      * @param args, not used
      */
     public static void main(String[] args) {
@@ -199,8 +236,8 @@ public class Person {
         Person persons[] = init();
 
         // iterate using "enhanced for loop"
-        for( Person person : persons) {
-            System.out.println(person);  // print object
+        for (Person person : persons) {
+            System.out.println(person); // print object
         }
     }
 
