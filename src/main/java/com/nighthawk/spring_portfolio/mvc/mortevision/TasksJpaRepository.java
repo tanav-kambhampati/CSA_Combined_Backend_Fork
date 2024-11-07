7d3c4691952e166ce2042374e7831a06bff7e5b2
SrijDude3416 <66487875+SrijDude3416@users.noreply.github.com>
2024-11-07 09:01:10 -0800

    added task manager stuff

--- src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/TasksJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/TasksJpaRepository.java
@@ -1,7 +1,9 @@
 package com.nighthawk.spring_portfolio.mvc.mortevision;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TasksJpaRepository extends JpaRepository<Tasks, Long> {
     // Additional query methods can be added here if needed
+    List<Tasks> findByUsernameOrderByDueDateAsc(String username);
 }
