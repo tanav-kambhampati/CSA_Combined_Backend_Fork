638e10dd92cb9a6be46a2f7800e48627fa107f66
eshaank1 <kumeshsd@gmail.com>
2024-11-12 22:44:14 -0800

    cors fixes

--- src/main/java/com/nighthawk/spring_portfolio/mvc/seedtracker/SeedApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/seedtracker/SeedApiController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://127.0.0.1:4100")
 @RestController
 @RequestMapping("/api/seeds")
 public class SeedApiController {
