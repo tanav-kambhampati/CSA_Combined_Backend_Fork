ee8d50c46956b4ca02ded0c551e5b8c298736a2a
Trystan-Schmits <142453909+Trystan-Schmits@users.noreply.github.com>
2024-11-21 12:07:45 -0800

    Update PersonApiController.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/PersonApiController.java
@@ -11,6 +11,7 @@ import org.json.simple.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
