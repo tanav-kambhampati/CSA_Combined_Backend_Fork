aa41c6478cad588cd00d97adbff8d7248763f1ef
Flying-Book <khobareanusha@gmail.com>
2024-11-21 16:27:47 -0800

    Added assigment priority to schema

--- src/main/java/com/nighthawk/spring_portfolio/Slack/CalendarEventRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/Slack/CalendarEventRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CalendarEventRepository extends JpaRepository<CalendarEvent, Long> {
     List<CalendarEvent> findByDate(LocalDate date);
     List<CalendarEvent> findByDateBetween(LocalDate startDate, LocalDate endDate);
+    List<CalendarEvent> findByType(String type);
 }
