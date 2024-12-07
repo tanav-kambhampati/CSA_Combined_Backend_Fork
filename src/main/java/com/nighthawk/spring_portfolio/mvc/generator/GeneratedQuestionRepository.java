734de3c9faffeafd26c481d403e954c70ea14321
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2024-12-06 20:52:39 -0800

    get rid of generator code (temporarily)

--- src/main/java/com/nighthawk/spring_portfolio/mvc/generator/GeneratedQuestionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.generator;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeneratedQuestionRepository extends JpaRepository<GeneratedQuestion, Long> {
-}
